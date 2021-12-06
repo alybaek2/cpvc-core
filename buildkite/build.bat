93af40014448b451eeff92b2b6a648b87b810259
adrian <alybaek2@gmail.com>
2021-12-05 21:31:33 -0800

    Initial test check-in for Github Actions.

--- buildkite/build.bat
+++ buildkite/build.bat
@@ -1,13 +1,5 @@
 %BUILD_NUGET% restore
 
-REM Generate dummy rom files.
-del roms\os6128.rom
-fsutil file createnew roms\os6128.rom 16384
-del roms\amsdos6128.rom
-fsutil file createnew roms\amsdos6128.rom 16384
-del roms\basic6128.rom
-fsutil file createnew roms\basic6128.rom 16384
-
 REM Build Debug and Release (x64).
 %BUILD_MSBUILD% cpvc-core.sln /t:Rebuild /p:Configuration=Release /p:Platform="x64"
 %BUILD_MSBUILD% cpvc-core.sln /t:Rebuild /p:Configuration=Debug /p:Platform="x64"
